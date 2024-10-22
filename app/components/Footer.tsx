82d1a5d1b90578c9bd534eb4406469a6babbf08a
divyam <singhdivyam772@gmail.com>
2024-10-22 11:02:00 +0530

    update the dyad

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -142,9 +142,9 @@ const Footer = () => {
           </div>
 
           {/* Subscription Section */}
-          {/* <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-            <h2 className="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-              SUBSCRIBE
+          <div className="lg:w-1/4 md:w-1/2 w-full px-4">
+            <h2 className="title-font font-medium text-gray-900 tracking-widest text-xl mb-3">
+              SEARCH
             </h2>
             <div className="flex xl:flex-nowrap md:flex-nowrap lg:flex-wrap flex-wrap justify-center items-end md:justify-start">
               <div className="relative w-40 sm:w-auto xl:mr-4 lg:mr-0 sm:mr-4 mr-2">
@@ -160,14 +160,14 @@ const Footer = () => {
                 />
               </div>
               <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
-                Subscribe
+                Search
               </button>
             </div>
             <p className="text-gray-500 text-sm mt-2 md:text-left text-center">
-              Bitters chicharrones fanny pack
-              <br className="lg:block hidden" /> waistcoat green juice
+              Discover more about our medical services and expertise.
+              <br className="lg:block hidden" /> Your health, our priority.
             </p>
-          </div> */}
+          </div>
         </div>
       </div>
 
